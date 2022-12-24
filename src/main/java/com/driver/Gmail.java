a48bd6573f97f3e5ca0d3d42cee368848d6b95d5
nikhil vanamala <nickhil215@gmail.com>
2022-12-24 22:09:41 +0530

    ohm

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -1,26 +1,6 @@
 package com.driver;
 
 import java.util.*;
-class info {
-    public Date data;
-    public String sender;
-    public String message;
-
-    public info(Date data, String sender, String message) {
-        this.data = data;
-        this.sender = sender;
-        this.message = message;
-    }
-    public Date getData() {
-        return data;
-    }
-    public String getSender() {
-        return sender;
-    }
-    public String getMessage() {
-        return message;
-    }
-}
 
 public class Gmail extends Email {
 

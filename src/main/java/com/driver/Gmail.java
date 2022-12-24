2d487e9113df60f7a1a87b22b553398000bfc173
nikhil vanamala <nickhil215@gmail.com>
2022-12-24 21:56:26 +0530

    nk

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -33,11 +33,11 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-      if(Inbox.size()<inboxCapacity){
-          Inbox.add(new info(date,sender,message));
-      }else{
-          Trush.add(new info(date,sender,message));
+      if(Inbox.size()==inboxCapacity){
+
+          Trush.add(Inbox.remove(0));
       }
+        Inbox.add(new info(date,sender,message));
     }
 
     public void deleteMail(String message){
@@ -48,9 +48,7 @@ public class Gmail extends Email {
             info obj=Inbox.get(i);
             if(obj.message.equals(message)){
                 Trush.add(new info(obj.data,obj.sender,obj.message));
-
                 Inbox.remove(obj);
-
             }
         }
     }
